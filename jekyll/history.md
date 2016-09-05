bef18ab63a86abca3c320ac72b7146e6a387e0d9
Daniel Skowroński <daniel@dsinf.net>
2016-09-05 20:44:42 +0200

    target look of decades navigator

--- jekyll/history.md
+++ jekyll/history.md
@@ -2,7 +2,7 @@
 layout: history
 permalink: /history/
 title: Historia (KSI40)
-excerpt: Nasze Koło ma już 40 lat! Zapoznaj się z jego historią.
+excerpt: Nasze Koło ma już 40 lat! Zapoznaj się z jego historią przeglądając kolejne dekady kronik.
 languages:
 - pl
 ---
--- jekyll/js/history.js
+++ jekyll/js/history.js
@@ -1,28 +1,31 @@
 /* scrips for ksi history displaying */
 
 function shouldBePosFixed(parent, child){
-  //console.log($(parent)[0].getBoundingClientRect().top);
-  //console.log($(parent)[0].getBoundingClientRect().top - $(parent).position().top + $(parent).height());
-
   var topBorderOk = $(parent)[0].getBoundingClientRect().top<100;
-  console.log(($(parent)[0].getBoundingClientRect().top - $(parent).position().top + $(parent).height()));
   var bottomBorderOk = ($(parent)[0].getBoundingClientRect().top - $(parent).position().top + $(parent).height()) > 100 /*$(child).height()*/;
-
 
   return topBorderOk && bottomBorderOk;
 }
 
 $( window ).bind('scroll', function() {
   var res = shouldBePosFixed($("#decade_viewer"), $("#decade_navigator"));
-  console.log(res);
   if (res){
     $("#decade_navigator").css('position', 'fixed')
     $("#decade_navigator").css('top', '100px')
-    //console.log("shouldBePosFixed");
   } else {
     $("#decade_navigator").css('position', 'absolute')
     $("#decade_navigator").css('top', 'auto')
-
-    //console.log("shouldNOTBePosFixed");
   }
+});
+
+function closeAllDecades(){
+  $(".decade").hide();
+}
+function openDecade(id){
+  closeAllDecades();
+  $("#decade_"+id).each(function(){ $(this).show(); })
+}
+
+$(function(){
+  openDecade("197");
 });
